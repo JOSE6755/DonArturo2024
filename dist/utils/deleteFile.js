50dd4c1a2123e89009b1d42202e48a4be865dbb1
Jose Martinez <joseamvi58@gmail.com>
2024-12-27 17:45:15 -0600

    Fix
    Fix

--- dist/utils/deleteFile.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.deleteFile = deleteFile;
-function deleteFile(path) {
-  try {
-  } catch (error) {}
-}
-//# sourceMappingURL=deleteFile.js.map
